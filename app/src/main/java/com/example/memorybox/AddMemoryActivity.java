53fa0cba9fa4c020ad4d22637583da6f56ad2877
JohanOh06 <90780186+JohanOh06@users.noreply.github.com>
2022-10-18 08:19:02 -0500

    10/18
    nice

--- app/src/main/java/com/example/memorybox/AddMemoryActivity.java
+++ app/src/main/java/com/example/memorybox/AddMemoryActivity.java
@@ -1,6 +1,7 @@
 package com.example.memorybox;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,6 +22,7 @@ public class AddMemoryActivity extends AppCompatActivity implements AdapterView.
     Spinner spinner;
     EditText memoryName, memoryDesc;
     String spinnerSelectedText = "none";
+    int memoryRatingNum;
 
 
     @Override
@@ -50,5 +52,37 @@ public class AddMemoryActivity extends AppCompatActivity implements AdapterView.
     @Override
     public void onNothingSelected(AdapterView<?> parent) { }
 
+    public void addMemoryButtonClicked(View view) {
+        String memName = memoryName.getText().toString();
+        String memDesc = memoryDesc.getText().toString();
+
+
+        //*** If you only have numbers in your string array, then don't include this part! *** //
+
+        //*** IF you have String text in your string array, then use a loop such as this ***//
+
+        // This loop take the option they clicked on and ensure it is a number.
+        // My options went from 5 to 1, so that is why I have it adjusted with 6-i
+        // I also had an instruction statement as my first line in my string array
+        // ADJUST THIS LOOP TO MATCH YOUR CODE!
+
+        // Note the syntax here for how to access an index of a string array within java
+        for (int i = 1; i < 5; i++) {
+            if (spinnerSelectedText.equals(getResources().getStringArray(R.array.memoryRating)[i])) {
+                memoryRatingNum = 5-i;
+                break;
+            }
+        }
+
+        // Everyone needs this part below this comment
+
+        Memory m = new Memory(memoryRatingNum, memName, memDesc);
+        SignInActivity.firebaseHelper.addData(m);
+
+        memoryName.setText("");
+        memoryDesc.setText("");
+    }
 
 }
+
+
